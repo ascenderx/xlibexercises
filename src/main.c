2040f8aea107bfd8a8c8bd2d5f8ddc9052c374eb
James Downer <downer.james.256k@gmail.com>
2020-09-04 02:55:20 -0600

    Doc comments in main

--- src/main.c
+++ src/main.c
@@ -11,23 +11,31 @@
 #define REFRESH_INTERVAL 1000
 
 int main() {
+  // Initialize data.
   struct MyXData xData;
   MyXData_initialize(&xData);
-  
   struct MyGame game;
   MyGame_initialize(&game, &xData);
 
   // Begin the main loop. 
   while (TRUE) {
+    // Update the window.
     MyXData_update(&xData);
+
+    // Update the game data.
     BOOL status = MyGame_update(&game);
     if (!status) {
       break;
     }
+
+    // Draw the game.
     MyGame_draw(&game);
+
+    // Sleep.
     usleep(REFRESH_INTERVAL);
   }
 
+  // Clean up.
   MyXData_finalize(&xData);
   printf("Goodbye.\n");
   return 0;
